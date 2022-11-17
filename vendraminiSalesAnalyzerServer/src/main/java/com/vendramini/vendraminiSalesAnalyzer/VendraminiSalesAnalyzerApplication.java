4aecb6c1923b4a9652aedb978bf7e137058f83fb
Rafael Vendramini <rafael.vendramini@ggbank.tech>
2022-11-16 21:26:15 -0300

    Creation of basic structure

---
+++ vendraminiSalesAnalyzerServer/src/main/java/com/vendramini/vendraminiSalesAnalyzer/VendraminiSalesAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.vendramini.vendraminiSalesAnalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendraminiSalesAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendraminiSalesAnalyzerApplication.class, args);
+	}
+
+}
---
+++ vendraminiSalesAnalyzerServer/src/test/java/com/vendramini/vendraminiSalesAnalyzer/VendraminiSalesAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vendramini.vendraminiSalesAnalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendraminiSalesAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
